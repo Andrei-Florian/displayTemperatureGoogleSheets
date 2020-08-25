88e86f56d1d79ab025c59ef58633b141bd7e5e15
Andrei Florian <andrei_florian@universumco.com>
2020-08-25 15:34:57 +0100

    hide credentials

--- displayTemperatureGoogleSheets/Accounts.h
+++ displayTemperatureGoogleSheets/Accounts.h
@@ -2,14 +2,14 @@
 
 // The user must edit all parameters below to their preference
 
-#define TEMBOO_ACCOUNT "lightpro"  // Your Temboo account name 
-#define TEMBOO_APP_KEY_NAME "UniversumYun"  // Your Temboo app key name
-#define TEMBOO_APP_KEY "xYcIuuRoAQ6TYNvbS8rBCZDferMzKScY"  // Your Temboo app key
+#define TEMBOO_ACCOUNT ""  // Your Temboo account name 
+#define TEMBOO_APP_KEY_NAME ""  // Your Temboo app key name
+#define TEMBOO_APP_KEY ""  // Your Temboo app key
 
-#define GOOGLE_CLIENT_ID "351437978877-nmmed7f0ucq0mtfei3ob1auds5lkhp9s.apps.googleusercontent.com"
-#define GOOGLE_CLIENT_SECRET "x7OlBaa3oDJdU0rZNIyYYNcT"
-#define GOOGLE_CLIENT_REFRESH_TOKEN "1/U1KQxPb8CJTdzSlV68V_GRH46II_53VQVIRBCE9M6_k"
-#define GOOGLE_SPREADSHEET_ID "1_tL4mS-phiXHiGdGEStBhSqaYaFyfwZkQnLerS6Mn8s"
+#define GOOGLE_CLIENT_ID ""
+#define GOOGLE_CLIENT_SECRET ""
+#define GOOGLE_CLIENT_REFRESH_TOKEN ""
+#define GOOGLE_SPREADSHEET_ID ""
 
 #if TEMBOO_LIBRARY_VERSION < 2
 #error "Your Temboo library is not up to date. You can update it using the Arduino library manager under Sketch > Include Library > Manage Libraries..."
